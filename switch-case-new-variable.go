9aeacf446e9966212d05e59cfa2739a9ad2e4f23
easywaldoji <easywaldoji@gmail.com>
2021-03-30 07:36:59 +0900

    Switch Case 문 안에서의 변수선언

---
+++ switch-case-new-variable.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	words := []string{"cow", "cat", "dog", "bird", "a", "elephant", "snake", "kangaroo", "development"}
+	for _, word := range words {
+		switch size := len(word); size {
+		case 1, 2, 3, 4:
+			fmt.Println(word, "is a short word")
+		case 5:
+			newLen := len(word)
+			fmt.Println(word, "is exactly the right length: ", newLen)
+		case 6, 7, 8, 9:
+		default:
+			fmt.Println(word, "is a long word")
+		}
+	}
+
+}
